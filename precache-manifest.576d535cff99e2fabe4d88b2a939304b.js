191a7afe9210bcdc12f0681537ce85e3bb052e75
TomGrooves <kasperfryd@gmail.com>
2020-03-30 11:49:23 +0200

    Updates

---
+++ precache-manifest.576d535cff99e2fabe4d88b2a939304b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed560e83cd5d4a7da9e22415b48ab2b2",
+    "url": "/contenful_blog/index.html"
+  },
+  {
+    "revision": "f1eef115ba15109e5496",
+    "url": "/contenful_blog/static/css/main.86013334.chunk.css"
+  },
+  {
+    "revision": "67534d138f46ded85fd4",
+    "url": "/contenful_blog/static/js/2.9baecf03.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/contenful_blog/static/js/2.9baecf03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1eef115ba15109e5496",
+    "url": "/contenful_blog/static/js/main.856d0de0.chunk.js"
+  },
+  {
+    "revision": "08d43f8e802dd1d58ab2",
+    "url": "/contenful_blog/static/js/runtime-main.24127544.js"
+  }
+]);
